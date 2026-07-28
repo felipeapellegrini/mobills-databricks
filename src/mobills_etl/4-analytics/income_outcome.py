583936725bbc985a84fe50d618248fdf27400fc3
Felipe A. Pellegrini <felipeapellegrini@gmail.com>
2026-07-28 16:40:05 -0300

    fix: change cdc key

--- src/mobills_etl/4-analytics/income_outcome.py
+++ src/mobills_etl/4-analytics/income_outcome.py
@@ -1,5 +1,5 @@
 from pyspark import pipelines as dp
-from pyspark.sql.functions import col, sum
+from pyspark.sql.functions import col, sum, when, lit
 
 _TRANSACOES = "gold.transacoes"
 _SNAPSHOT = "_src_income_outcome"
@@ -14,12 +14,12 @@ def _source():
         .filter((col("ano_mes") >= "2026-01-01") & (col("natureza").isin(["Despesas", "Receitas"])))
         .groupBy(["ano_mes", "natureza"])
         .agg(sum(col("valor_abs")).alias("valor_abs"))
-        .orderBy("ano_mes", "natureza")
+        .withColumn("despesas", when(col("natureza") == "Despesas", col("valor_abs")).otherwise(lit(0)))
+        .withColumn("receitas", when(col("natureza") == "Receitas", col("valor_abs")).otherwise(lit(0)))
+        .select("ano_mes", "despesas", "receitas")
     )
 
 
 dp.create_streaming_table(name=_ANALYTICS)
 
-dp.create_auto_cdc_from_snapshot_flow(
-    target=_ANALYTICS, source=_SNAPSHOT, keys=["ano_mes", "natureza"], stored_as_scd_type=1
-)
+dp.create_auto_cdc_from_snapshot_flow(target=_ANALYTICS, source=_SNAPSHOT, keys=["ano_mes"], stored_as_scd_type=1)
